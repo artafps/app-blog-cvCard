dece30a4968b0d6e285cc3fc497a974b033486b8
artafp <artafallahpoor@gmail.com>
2024-07-23 20:56:47 +0330

    end blogs

--- src/components/admin/Dashboard/Blogs.js
+++ src/components/admin/Dashboard/Blogs.js
@@ -1,6 +1,183 @@
 
+import axios from "axios";
 import Dashboard from "../dashboard";
+import { Octokit } from "@octokit/rest";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 const  Blogs= () => {
-    return ( <Dashboard title='Blogs' disc='Editing the blog section'>'Blogs' </Dashboard>);
+    
+    const handleGetFileContent = async (fileUrl) => {
+        try {
+            const response = await axios.get(fileUrl);
+            return response.data; // محتوای فایل در اینجا بازگردانده می‌شود
+        } catch (error) {
+            console.error('Error fetching file content:', error);
+            throw error; // ارور ممکن است برای مدیریت خطاها بازگردانده شود
+        }
+    };
+
+    const handleDelete = (path, sha) => {
+        const octokit = new Octokit({
+            auth: accessToken
+        });
+        const owner = OwnerName;
+        const repo = RepoName;
+        octokit.repos.deleteFile({
+            owner: owner,
+            repo: repo,
+            path: path,
+            message: "Delete file",
+            sha: sha
+        }).then(response => {
+            console.log("File deleted:", response.data);
+            toast.success('File Deleted Successfully!')
+        }).catch(err => {
+            console.error("Error deleting file:", err);
+            toast.error('Error deleting file!')
+        });
+    };
+
+
+   
+
+
+    const configNameFile = 'Config-Web-EN.json'
+
+    const accessToken = localStorage.getItem('AC');
+    const OwnerName = localStorage.getItem('Owner');
+    const RepoName = localStorage.getItem('Repo');
+
+    const [ONCHANGESAVE, setONCHANGESAVE] = useState(false);
+    const [Sha, setSha] = useState('');
+    const [DATAGITARRY, setDATAGITARRY] = useState();
+
+
+    const handleGetFileData = (file) => {
+        const filePath = `https://api.github.com/repos/${OwnerName}/${RepoName}/contents/${file.path}`;
+        handleGetFileContent(filePath).then((res) => {
+            const binaryData = decodeURIComponent(escape(atob(res.content)));
+            localStorage.setItem('DATAGITBACK', binaryData)
+            const DATAGIT = JSON.parse(binaryData)
+            setDATAGITARRY(DATAGIT)
+            console.log(DATAGIT)
+            setSha(res.sha)
+           
+            console.log('File content:', res);
+        })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+    const getData = () => {
+        const octokit = new Octokit({
+            auth: accessToken
+        });
+        const owner = OwnerName;
+        const repo = RepoName;
+
+        octokit.paginate(`GET /repos/${OwnerName}/${RepoName}/contents/configs`, {
+            owner: owner,
+            repo: repo,
+            path: ""
+        }).then(files => {
+            console.log(files)
+            files.map(item => {
+                if (item.name === configNameFile) {
+                    handleGetFileData(item)
+                }
+            })
+        }).catch(err => {
+            console.error("Error getting file list:", err);
+        });
+    }
+    useEffect(() => {
+        getData()
+    }, []);
+    const editFile = async (fileName, newContent, sha, token) => {
+        try {
+            const filePath = `https://api.github.com/repos/${OwnerName}/${RepoName}/contents/configs/${fileName}`
+            const response = await axios.put(filePath, {
+                message: 'Edit file',
+                content: btoa(unescape(encodeURIComponent(newContent))),
+                sha: sha
+            }, {
+                headers: {
+                    Authorization: `token ${token}`
+                }
+            });
+            toast.success('File edited successfully!')
+            return response.data;
+        } catch (error) {
+            console.error('Error editing file:', error);
+            toast.error('Error editing file!')
+            throw error;
+        }
+    };
+    const HANDLESAVE = async () => {
+        await editFile(configNameFile, localStorage.getItem('DATAGITBACK'), Sha, accessToken)
+        setONCHANGESAVE(false)
+        await setTimeout(async () => {
+            getData()
+        }, 4000);
+        setTimeout(async () => {
+            const Data = localStorage.getItem('DATAGITBACK')
+            if (JSON.stringify(DATAGITARRY) !== Data) {
+                //bug is here 
+                setONCHANGESAVE(false)
+            } else {
+                setONCHANGESAVE(false)
+            }
+        }, 5000);
+    }
+
+    const [TEXT, setTEXT] = useState("");
+    const handleChangeBLOGTITLE = async (events) => {
+        let Data = localStorage.getItem('DATAGITBACK')
+        if (Data === null) {
+            DATAGITARRY.Blog.Title = String(events)
+            localStorage.setItem('DATAGITBACK', JSON.stringify(DATAGITARRY))
+            setTEXT(events)
+        } else {
+            const newData = JSON.parse(Data)
+            newData.Blog.Title = String(events)
+            localStorage.setItem('DATAGITBACK', JSON.stringify(newData))
+            setTEXT(events)
+        }
+        Data = localStorage.getItem('DATAGITBACK')
+        if (JSON.stringify(DATAGITARRY) !== Data) {
+            setONCHANGESAVE(true)
+        } else {
+            setONCHANGESAVE(false)
+        }
+    }
+
+
+    return ( <Dashboard title='Blogs' disc='Editing the blog section'>
+  {ONCHANGESAVE ? <div class="card" style={{ left: 500, bottom: 20, boxShadow: "5px 5px 100px gray", width: 700, position: 'fixed', zIndex: 10 }}>
+            <div class="card-body" style={{ display: "flex", justifyContent: 'space-between' }}><div>Click the Save button to save the information ؟</div>
+                <div>
+                    <button onClick={HANDLESAVE} type="button" class="btn btn-outline-success m-2">Save</button>
+                    <button onClick={() => {
+                        getData()
+                        setONCHANGESAVE(false)
+                    }} type="button" class="btn btn-outline-danger">Return</button></div></div></div>
+            : null}
+
+<div className="card">
+            <div className=" widget widget-payment-request"><div className="card-header">
+                <h5 className="card-title">Blog Title</h5>
+            </div>
+                <div className="card-body">
+                    
+                    <br />
+                    <label class="form-check-label" >Blog Title </label>
+                    <input value={TEXT} onChange={e => handleChangeBLOGTITLE(e.target.value)} type="text" class="form-control" placeholder="Blog Title " aria-label="Username" aria-describedby="basic-addon1" />
+
+                    
+
+                </div>
+            </div>
+        </div>
+    </Dashboard>);
 }
 export default Blogs;
--- src/components/admin/Dashboard/LanguageBoxS.js
+++ src/components/admin/Dashboard/LanguageBoxS.js
@@ -152,7 +152,7 @@ const LanguageBoxS = () => {
             }
         }, 5000);
     }
-
+// ------------------------
     const [STATUSSKILS, setSTATUSSKILS] = useState(false);
     const [SKILSARR, setSKILSARR] = useState([]);
     const [TextI, setTextI] = useState('');
