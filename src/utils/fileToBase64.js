1e2ac2241f3762f23c39929b47ca25ab1921bbb5
artafp <artafallahpoor@gmail.com>
2024-07-30 11:55:21 +0330

    uploading

--- src/utils/fileToBase64.js
+++
@@ -1,8 +0,0 @@
-export function fileToBase64(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result.split(',')[1]);
-        reader.onerror = error => reject(error);
-    });
-}
